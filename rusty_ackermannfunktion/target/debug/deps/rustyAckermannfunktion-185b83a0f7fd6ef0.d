90e163f217af7718964e466b638db842e3182e0a
jojo <johannes.zoeller@gmail.com>
2023-01-17 15:50:13 +0100

    IDE doesn't like it that way

--- rusty_ackermannfunktion/target/debug/deps/rustyAckermannfunktion-185b83a0f7fd6ef0.d
+++
@@ -1,5 +0,0 @@
-/home/jojo/IdeaProjects/rusty_ackermannfunktion/target/debug/deps/rustyAckermannfunktion-185b83a0f7fd6ef0: src/main.rs
-
-/home/jojo/IdeaProjects/rusty_ackermannfunktion/target/debug/deps/rustyAckermannfunktion-185b83a0f7fd6ef0.d: src/main.rs
-
-src/main.rs:
--- rusty_ackermannfunktion/target/debug/rustyAckermannfunktion.d
+++
@@ -1,1 +0,0 @@
-/home/jojo/IdeaProjects/rusty_ackermannfunktion/target/debug/rustyAckermannfunktion: /home/jojo/IdeaProjects/rusty_ackermannfunktion/src/main.rs
