d3596fbf0896c6c374176ff1fd4b46180ab6b2e9
zplays <32310711+zplays@users.noreply.github.com>
2023-01-17 15:45:25 +0100

    Added files from local

---
+++ rusty_ackermannfunktion/target/debug/deps/rustyAckermannfunktion-185b83a0f7fd6ef0.d
@@ -0,0 +1,5 @@
+/home/jojo/IdeaProjects/rusty_ackermannfunktion/target/debug/deps/rustyAckermannfunktion-185b83a0f7fd6ef0: src/main.rs
+
+/home/jojo/IdeaProjects/rusty_ackermannfunktion/target/debug/deps/rustyAckermannfunktion-185b83a0f7fd6ef0.d: src/main.rs
+
+src/main.rs:
---
+++ rusty_ackermannfunktion/target/debug/rustyAckermannfunktion.d
@@ -0,0 +1,1 @@
+/home/jojo/IdeaProjects/rusty_ackermannfunktion/target/debug/rustyAckermannfunktion: /home/jojo/IdeaProjects/rusty_ackermannfunktion/src/main.rs
